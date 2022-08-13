e6f82ee87edc45f08bc057f3750c9726e937a718
EAZYLINK <ezekiel.nwuguru@eng.uniben.edu>
2022-08-13 11:12:26 +0100

    Update Config.java

--- src/DBConnection/Config.java
+++ src/DBConnection/Config.java
@@ -5,7 +5,7 @@ public class Config {
 	protected static String dbhost = "localhost";
 	protected static String dbport = "3306";
 	protected static String dbusername = "root";
-	protected static String dbpassword = "eazylink.net";
+	protected static String dbpassword = "";
 	protected static String dbname = "mysupermarket";
 	
 
